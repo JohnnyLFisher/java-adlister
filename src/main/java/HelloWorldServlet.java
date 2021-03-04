551f17c38d898c57d2f88340e2f4b33a8c8c8542
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-03-04 14:39:27 -0600

    Did bonus

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -3,14 +3,19 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<h1>Hello, World!</h1>");
-    }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		if (name != null) {
+			out.printf("<h1>Hello, %s!</h1>", name);
+		} else {
+			out.println("<h1>Hello, World!</h1>");
+		}
+	}
 
 }
